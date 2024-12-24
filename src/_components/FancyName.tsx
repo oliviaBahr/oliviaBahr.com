12dd5f136a267dcd4181fb261aa6c1d1feb121e0
Olivia Bahr <ob.bahr@gmail.com>
2024-12-24 15:01:50 -0700

    move to vento

--- src/_components/FancyName.tsx
+++
@@ -1,7 +0,0 @@
-export default function FancyName() {
-  return (
-    <h1 className="text-6xl ml-6 m-4 shrikhand rainbow-shadows">
-      Olivia<br />Bahr
-    </h1>
-  );
-}
--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-// Redirect to the about page
-export default function HomePage() {
-  return (
-    <div>
-      <meta httpEquiv="refresh" content="0;url=/pages/about/" />
-    </div>
-  );
-}
