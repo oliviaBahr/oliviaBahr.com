b5b016f5320c48489fba38d23f114db18673be6c
Olivia Bahr <ob.bahr@gmail.com>
2024-12-29 19:24:58 -0700

    update theme

--- _config.js
+++ _config.js
@@ -5,7 +5,6 @@ import code_highlight from "lume/plugins/code_highlight.ts";
 import google_fonts from "lume/plugins/google_fonts.ts";
 import tailwindcss from "lume/plugins/tailwindcss.ts";
 import postcss from "lume/plugins/postcss.ts";
-import typography from "npm:@tailwindcss/typography";
 import redirects from "lume/plugins/redirects.ts";
 import icons from "lume/plugins/icons.ts";
 import highlight from "lume/plugins/code_highlight.ts";
@@ -34,10 +33,6 @@ site.use(
     cssFile: "styles/main.css",
     fonts: {
       text: "https://fonts.googleapis.com/css2?family=Sono:wght@200..800&display=swap",
-      display:
-        "https://fonts.googleapis.com/css2?family=Karla:ital,wght@0,200..800;1,200..800&display=swap",
-      yeseva:
-        "https://fonts.googleapis.com/css2?family=Yeseva+One&display=swap",
       shrikhand:
         "https://fonts.googleapis.com/css2?family=Shrikhand&display=swap",
     },
@@ -46,29 +41,21 @@ site.use(
 site.use(
   tailwindcss({
     content: ["./src/**/*.vto"],
-    plugins: [typography],
     options: {
       theme: {
         extend: {
           colors: {
-            // Base tones
-            stone: {
-              950: "#100F0F", // black
-              900: "#1C1B1A",
-              850: "#282726",
+            paper: {
+              50: "#FFFCF0",
+              100: "#F9F5E9",
+              200: "#F2F0E5",
+              300: "#E6E4D9",
+              400: "#DAD8CE",
+              500: "#CECDC3",
+              600: "#B7B5AC",
+              700: "#6F6E69",
               800: "#343331",
-              750: "#403E3C",
-              700: "#575653",
-              600: "#6F6E69",
-              500: "#878580",
-              300: "#B7B5AC",
-              200: "#CECDC3",
-              150: "#DAD8CE",
-              100: "#E6E4D9",
-              50: "#F2F0E5",
-              paper: "#FFFCF0",
             },
-            // Dark colors
             dark: {
               red: "#AF3029",
               orange: "#BC5215",
@@ -79,18 +66,6 @@ site.use(
               purple: "#5E409D",
               magenta: "#A02F6F",
             },
-            paper: {
-              0: "#FFFCF0",
-              10: "#F9F5E9",
-              20: "#F2F0E5",
-              30: "#E6E4D9",
-              40: "#DAD8CE",
-              50: "#CECDC3",
-              60: "#B7B5AC",
-              70: "#6F6E69",
-              80: "#343331",
-            },
-            // Light colors
             light: {
               red: "#D14D41",
               orange: "#DA702C",
